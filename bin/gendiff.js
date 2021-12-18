9039e4e0239acfc553a64046d4e014f9da0e65ad
Artem Stupin <artm731@gmail.com>
2021-12-18 18:27:31 +0400

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
-const program = new Command();
+import genDiff from '../src/generateDiff.js';
 
-program 
-  .version('1.0.0', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
-
-program.parse();
+genDiff();
---
+++ src/generateDiff.js
@@ -0,0 +1,28 @@
+import { program } from 'commander/esm.mjs';
+import readJsonFile from './readJsonFile.js';
+import showDiff from './findDiff.js';
+
+const genDiff = () => {
+  program
+    .version('1.0.0', '-V, --version', 'output the version number')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+      if (filepath1.endsWith('.json') && filepath2.endsWith('json')) {
+        const file1Content = readJsonFile(filepath1);
+        const file2Content = readJsonFile(filepath2);
+
+        const difference = showDiff(file1Content, file2Content);
+
+        console.log(difference);
+      } else {
+        console.log('wrong format of files');
+      }
+    });
+
+  program.parse();
+};
+
+export default genDiff;
